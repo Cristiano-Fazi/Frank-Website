fc0b7b6ccececb0473e2d54a8c6e86ba1bf6dbcb
Cristiano Fazi <cristianofazi03@gmail.com>
2025-07-07 20:01:02 -0400

    added submission page to router
    added page

---
+++ frank-website/src/pages/submission/Submission.jsx
@@ -0,0 +1,9 @@
+// import { useState } from 'react';
+// import styles from './Submission.module.css';
+
+export default function Submission() {
+
+    return (
+        <p> Submission Page goes here </p>
+    )
+}
