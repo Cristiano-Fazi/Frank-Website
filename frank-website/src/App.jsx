f9f88ffee876500ddd9b58338a22db7d2518d67f
Cristiano Fazi <cristianofazi03@gmail.com>
2025-07-09 18:35:42 -0400

    Fix capital letters mismatch for Navbar
    The typo in the capitalization was breaking the Linux build on cloudflare

--- frank-website/src/App.jsx
+++ frank-website/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Footer from './components/Footer/Footer.jsx' 
-import Navbar from './components/NavBar/NavBar.jsx'
+import Navbar from './components/Navbar/Navbar.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
