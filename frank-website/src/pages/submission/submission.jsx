3e2355cb5507d2988cb58e727dbd5ac2a370384c
Cristiano Fazi <cristianofazi03@gmail.com>
2025-07-10 13:44:31 -0400

    changes

--- frank-website/src/pages/submission/submission.jsx
+++
@@ -1,9 +0,0 @@
-// import { useState } from 'react';
-// import styles from './Submission.module.css';
-
-export default function Submission() {
-
-    return (
-        <p> Submission Page goes here </p>
-    )
-}
