e7427477819e2c97d8ef06a680fc2886736cb0ed
Alyssa360 <garofalo.alyssa33@gmail.com>
2025-08-26 12:17:52 -0400

    Home top img
    components for the image of about us page

---
+++ frank-website/src/components/HomeTop/HomeTop.jsx
@@ -0,0 +1,16 @@
+import homeImg from "../../assets/Frank-front.png";
+import styles from "./HomeTop.module.css";
+
+export default function HomeTop() {
+  return (
+    <div className={styles.homePage_container}>
+      <img src={homeImg} alt="tree picture" className={styles.homeMain_img} />
+
+      <div className={styles.homeMain_text}>
+        <h1>Arborist Certified and Passionate</h1>
+        <p className="service_urgence">Urgent Service available 24h/7</p>
+        <p className="phone_number">514-651-7659</p>
+      </div>
+    </div>
+  );
+}
