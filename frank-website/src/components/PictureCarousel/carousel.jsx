73b897d7cc4a3d0f7216a15c4000c5a662c9433f
Alyssa360 <garofalo.alyssa33@gmail.com>
2025-09-02 10:32:40 -0400

    picture carousel
    still need to make the arrows nicer

--- frank-website/src/components/PictureCarousel/carousel.jsx
+++ frank-website/src/components/PictureCarousel/carousel.jsx
@@ -1,9 +1,52 @@
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 import styles from "./carousel.module.css";
 import OneCar from "../../assets/OneCar.png";
 import twoCar from "../../assets/twoCar.png";
-import threeCar from "../../assets/threeCar.png"
+import threeCar from "../../assets/threeCar.png";
 
-export default function carousel(){
-    
+const NextArrow = ({ onClick }) => (
+  <div className={styles.nextArrow} onClick={onClick}>›</div>
+);
 
-}
+const PrevArrow = ({ onClick }) => (
+  <div className={styles.prevArrow} onClick={onClick}>‹</div>
+);
+
+const Carousel = () => {
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    autoplay: false,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          arrows: true,
+        },
+      },
+    ],
+  };
+
+  return (
+    <div className={styles.carouselWrapper}>
+      <Slider {...settings}>
+        <div><img src={OneCar} alt="One Car" className={styles.carouselImage} /></div>
+        <div><img src={twoCar} alt="Two Car" className={styles.carouselImage} /></div>
+        <div><img src={threeCar} alt="Three Car" className={styles.carouselImage} /></div>
+      </Slider>
+    </div>
+  );
+};
+
+export default Carousel;
--- frank-website/src/pages/about/aboutUs.jsx
+++ frank-website/src/pages/about/aboutUs.jsx
@@ -1,13 +1,16 @@
 import HomeTop from '../../components/HomeTop/HomeTop.jsx'
 import HomeDes from '../../components/HomeDescription/HomeDes.jsx'
 import Services from '../../components/HomeServices/services.jsx'
+import Carousel from '../../components/PictureCarousel/carousel.jsx'
 
 export default function About(){
     return(
         <div>
             <HomeTop/>
             <HomeDes/>
+            <Carousel/>
             <Services/>
+            
         </div>
     )
 }
